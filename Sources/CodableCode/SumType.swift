4852682dd6755b3e07684b3307fdc382e0151424
Julio Fruta <julio.guzman@me.com>
2023-10-10 23:19:24 -0700

    Update SumType.swift

--- Sources/CodableCode/SumType.swift
+++ Sources/CodableCode/SumType.swift
@@ -17,4 +17,33 @@ struct SumType: Equatable, Hashable {
         code += ["}"]
         return code
     }
+    
+    // TODO: Add the following case.
+    // enum Your: Codable {
+    //     case integer(Int)
+    //     case string(String)
+    //
+    //     init(from decoder: Decoder) throws {
+    //         let container = try decoder.singleValueContainer()
+    //         if let x = try? container.decode(Int.self) {
+    //             self = .integer(x)
+    //             return
+    //         }
+    //         if let x = try? container.decode(String.self) {
+    //             self = .string(x)
+    //             return
+    //         }
+    //         throw DecodingError.typeMismatch(Your.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for Your"))
+    //     }
+    //
+    //     func encode(to encoder: Encoder) throws {
+    //         var container = encoder.singleValueContainer()
+    //         switch self {
+    //         case .integer(let x):
+    //             try container.encode(x)
+    //         case .string(let x):
+    //             try container.encode(x)
+    //         }
+    //     }
+    // }
 }
