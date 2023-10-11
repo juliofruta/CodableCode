a9052f33a4bb2aff8458dcf6db7e32802e947b8d
Julio Fruta <julio.guzman@me.com>
2023-10-10 22:16:23 -0700

    wip

--- Sources/CodableCode/String.swift
+++ Sources/CodableCode/String.swift
@@ -1,11 +1,6 @@
 import Foundation
 import Algorithms
 
-
-
-
-
-
 extension String {
     
     /// Uppercases the first character
@@ -93,11 +88,3 @@ extension String {
     }
 }
 
-/// Function used for printing.
-/// - Parameters:
-///   - file: The file at which the function was called from.
-///   - line: The line at whic the function was called from.
-///   - output: The string to be printed.
-func dump2(file: String = #file, line: Int = #line, _ output: String) {
-    print(">>> \(output)\t|\(file):\(line)")
-}
